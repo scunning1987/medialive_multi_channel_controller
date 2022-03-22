fc6ace51e493b83bda0027f9ec7cca1eaaa7cb5f
scott <cunsco@amazon.com>
2022-03-21 21:05:39 -0600

    Tue Mar 22 03:05:39 UTC 2022

--- step-lambda-medialivechannel.py
+++ step-lambda-medialivechannel.py
@@ -25,6 +25,11 @@ def lambda_handler(event, context):
     dynamodb_table_name= event['detail']['dynamodb_table_name']
     channel_data = event['detail']['channel_data']
 
+    max_resolution = "HD"
+    max_bitrate = "MAX_10_MBPS"
+    input_codec = "AVC"
+    loglevel = "DISABLED"
+
 
     ###
     ### FUNCTIONS
@@ -75,7 +80,7 @@ def lambda_handler(event, context):
 
         # Create Channel
         try:
-            create_channel_response = eml_client.create_channel(ChannelClass=pipeline,InputAttachments=input_attachments,Destinations=destinations_template,EncoderSettings=encoder_settings,InputSpecification={'Codec':input_codec,'MaximumBitrate':max_bitrate,'Resolution':max_resolution},LogLevel=loglevel,Name=channel_name,RoleArn=role_arn)
+            create_channel_response = eml_client.create_channel(ChannelClass=pipeline,InputAttachments=channel_input_attachments,Destinations=destinations_template,EncoderSettings=encoder_settings,InputSpecification=input_specs,LogLevel=loglevel,Name=channel_name,RoleArn=role_arn)
         except Exception as e:
             LOGGER.error("Unable to create channel %s, got exception : %s" % (channel_name,e))
             exceptions.append("Unable to create channel, got exception : %s" % (e))
@@ -142,6 +147,7 @@ def lambda_handler(event, context):
     def dynamo_to_json(dicttopopulate,my_dict):
         for k,v in my_dict.items():
 
+
             value_type = list(my_dict[k].keys())[0]
 
             if value_type == "M":
@@ -163,13 +169,18 @@ def lambda_handler(event, context):
                 value = my_dict[k][value_type]
 
                 new_item_list = []
+                new_item_list.clear()
 
-                dynamodb_item_list = dict()
+
+
                 for i in range(0,len(value)):
+
+                    dynamodb_item_list = dict()
+                    dynamodb_item_list.clear()
 
                     dynamo_to_json(dynamodb_item_list,value[i])
 
-                new_item_list.append(dynamodb_item_list)
+                    new_item_list.append(dynamodb_item_list)
 
                 dicttopopulate.update({k:new_item_list})
 
@@ -197,15 +208,15 @@ def lambda_handler(event, context):
     json_item = dict()
     dynamo_to_json(json_item,db_item)
 
+    # initialize medialive boto3 client
+    region = json_item['Region']
+    eml_client = boto3.client('medialive', region_name=region)
+
     if task == "create":
 
         medialive_db_item = dict()
 
         pipeline = json_item['Pipeline']
-        region = json_item['Region']
-
-        # initialize medialive boto3 client
-        eml_client = boto3.client('medialive', region_name=region)
 
         # task = event['detail']['task']
         # channels = event['detail']['channels']
@@ -213,6 +224,8 @@ def lambda_handler(event, context):
         # deployment_name = event['detail']['name']
         # dynamodb_table_name
 
+        jpg_bucket = os.environ['S3Bucket']
+        jpg_base_path = '/'.join(os.environ['FrameCaptureToJpgPath'].split("/")[0:-1])
 
         # Get MediaLive templates to json dictionaries
         template_bucket = os.environ['S3Bucket']
@@ -221,6 +234,8 @@ def lambda_handler(event, context):
         mux_sd_avc = get_template_from_s3(template_bucket,os.environ['MuxSDTemplate'])
         ott_hd_avc = get_template_from_s3(template_bucket,os.environ['OttSDTemplate'])
         ott_sd_avc = get_template_from_s3(template_bucket,os.environ['OttSDTemplate'])
+
+        default_template = ott_sd_avc
 
         if len(exceptions) > 0:
 
@@ -270,33 +285,104 @@ def lambda_handler(event, context):
             LOGGER.info("No granular details sent with group create. Creating OTT channels only")
             LOGGER.info("Defaulting to create SD OTT channels only")
 
-            return "..yeah.."
+            for channel in range(1,int(channels)+1):
 
-            #
-            #
-            # Need to just build an output here. no channel_data to pull from, do a cookie cutter channel creation
-            #
-            #
+                input_specs = dict()
+                input_specs['Codec'] = input_codec
+                input_specs['MaximumBitrate'] = max_bitrate
+                input_specs['Resolution'] = max_resolution
+
+                medialive_channel = json_item["MediaLive"][str(channel)]
+
+                encoder_settings = default_template['EncoderSettings']
+                destinations_template = default_template['Destinations']
+
+                # InputAttachmentName, InputId, InputSettings
+                channel_input_attachments = []
+                for ia in range(0,len(medialive_channel['Input_Attachments'])):
+
+                    input_properties = {
+                        "InputAttachmentName":medialive_channel['Input_Attachments'][ia]['Name'],
+                        "InputId":medialive_channel['Input_Attachments'][ia]['Id'],
+                        "InputSettings":{
+                            "SourceEndBehavior":""
+                        }
+
+                    }
+
+
+                    if "continue" in medialive_channel['Input_Attachments'][ia]['Name'].lower():
+                        input_properties['InputSettings']['SourceEndBehavior'] = "LOOP"
+                    else:
+                        input_properties['InputSettings']['SourceEndBehavior'] = "CONTINUE"
+
+                    if medialive_channel['Input_Attachments'][ia]['Type'] == "URL_PULL" and "m3u8" in medialive_channel['Input_Attachments'][ia]['Sources'][0]['Url']:
+                        input_properties['InputSettings']['NetworkInputSettings'] = {"HlsInputSettings":{"BufferSegments":3,"Scte35Source":"MANIFEST"}}
+
+                    channel_input_attachments.append(input_properties)
+
+                # construct medialive event outputs
+                for destination in destinations_template:
+                    if 'MediaPackageSettings' in destination: # This output is to MediaPackage
+                        LOGGER.debug("This is MediaPackage destination - EMP: %s" % (destination))
+
+                        mediapackage_id = json_item['MediaPackage'][str(channel)]['Channel_Name']
+
+                        destination['MediaPackageSettings'][0]['ChannelId'] = mediapackage_id # Path to put MediaPackage Channel ID
+
+                    elif 'Settings' in destination:
+                        LOGGER.debug("This is S3 Destination - JPG: %s" % (destination))
+                        # ottauto/deployments
+                        jpg_full_path = "s3://%s/%s/%s/%s/status" % (jpg_bucket,jpg_base_path,deployment_name,channel)
+                        LOGGER.debug("MediaLive Channel %s outputting JPG to %s" % (channel, jpg_full_path))
+                        destination['Settings'][0]['Url'] = jpg_full_path # Path to s3 published JPG
+
+                channel_name = "%s_%02d" % (deployment_name,channel)
+
+
+                medialive_create_channel_response = createEMLChannel()
+
+                if len(exceptions) > 0:
+                    errorOut()
+
+                channel_arn = medialive_create_channel_response['Channel']['Arn']
+
+                # Update json item
+                json_item["MediaLive"][str(channel)]['Channel_Name'] = channel_name
+                json_item["MediaLive"][str(channel)]['Channel_Arn'] = channel_arn
+
+
+        eml_dict_to_dynamo = dict()
+        json_to_dynamo(eml_dict_to_dynamo,json_item)
+
+        putItem(eml_dict_to_dynamo)
+        if len(exceptions) > 0:
+            return errorOut()
+        else:
+            event['status'] = "Completed creation of MediaLive channels with no issues"
+            return event
 
 
     else: # this is delete
 
-        return json_item
+        if event['detail']['delete_tasks']['medialive_channels'] == 0:
 
-        medialive_channels = json_item['MediaLive']
+            medialive_channels = json_item['MediaLive']
 
-        deleted_channels = []
-        failed_deleted_channels = []
-        for channel in list(medialive_channels.keys()):
+            delete_exceptions = []
+            for channel in list(medialive_channels.keys()):
 
-            channel_id = medialive_channels[channel]['ChannelId']
+                try:
+                    channel_arn = medialive_channels[channel]['Channel_Arn']
+                    channel_id = channel_arn.split(":")[-1]
+                    deleteEMLChannel(channel_id)
 
-            # send to channel delete
-            # delete function
-            # if no exception, add to deleted_channels list
+                except:
 
-            # catch exceptions but dont error
+                    delete_exceptions.append("Unable to get MediaLive Channel Arn info from DB for channel : %s " % (channel_id))
 
-            # add status for MediaLive inputs to use
-            event['medialive_channels_deleted'] = []
-            event['medialive_channel_delete_failed'] = failed_deleted_channels
+            event['status'] = "Completed deletion of MediaLive Channels"
+            event['eml_channel_delete_exceptions'] = delete_exceptions
+            event['detail']['delete_tasks']['medialive_channels'] = 1
+
+        return event
